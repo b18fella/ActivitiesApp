4aed2d91a6403c2a982e2a855aefce534d650ddd
b18fella <b18fella@student.his.se>
2019-04-30 14:37:10 +0200

    Card is fixed. Constraint layout implemented. Project done!

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
-    // Create ArrayLists from the raw data above and use these lists when populating your ListView.
     private ArrayList<String> listData=new ArrayList<>(Arrays.asList(mountainNames));
     private ArrayList<Mountain> mountainArrayList = new ArrayList<>();
 
@@ -37,26 +36,6 @@ public class MainActivity extends AppCompatActivity {
         mountainArrayList.add(new Mountain("Mont Blanc", "Alps", 4808));
         mountainArrayList.add(new Mountain("Denali", "Alaska", 6190));
         Log.i("LAAKSO",mountainArrayList.get(0).getName());
-
-        // 1. Create a ListView as in previous assignment
-        // 2. Create a new activity named "MountainDetailsActivity
-        // 3. Create a new Layout file for the MountainDetailsActivity called
-        //    "activity_mountaindetails". MountainDetailsActivity must have MainActivity as its
-        //    ´parent activity.
-        // 4. The layout file created in step 3 must have a LinearLayout
-        // 5. The layout file created in step 3 must be able to display
-        //    * Mountain Name
-        //    * Mountain Location
-        //    * Mountain Height
-        // 6. When tapping on a list item: create an Intent that includes
-        //    * Mountain Name
-        //    * Mountain Location
-        //    * Mountain Height
-        // 7. Display the MountainDetailsActivity with the data from the Intent created in step
-        //    6
-        // 8. From the MountainDetailsActivity you should have an option to "go back" using an
-        //    left arro button. This is done by letting the MainActivity be the parent activity to
-        //    MountainDetailsActivity.
 
         ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this, R.layout.list_item_textview, R.id.list_item_textview, mountainArrayList);
 
